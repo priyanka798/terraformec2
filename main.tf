e3818a7e4a14538b3f39fca2e40dc4fcd02ace29
priyankaraytech <83564724+priyanka798@users.noreply.github.com>
2024-07-29 20:29:53 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,10 +9,14 @@ terraform {
 
 provider "google" {
   project = "polar-office-422517-t9"
-  region  = "us-central1"  # Specify the region for resources
+  region  = "us-central1"  # Specify the region if needed for other resources
 }
 
-resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = true  # Automatically create default subnetworks
+resource "google_storage_bucket" "my_bucket" {
+  name     = "bucket251094"  # Must be globally unique
+  location = "US"                      # Location of the bucket
+
+  lifecycle {
+    prevent_destroy = true             # Optional: Prevent the bucket from being accidentally destroyed
+  }
 }
